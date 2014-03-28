00d5e8deee43f711270d1053513029d139384d39
Tarvitz <tarvitz@blacklibrary.ru>
2014-03-28 20:18:26 +0400

    add setup file

--- dsfp/__init__.py
+++ dsfp/__init__.py
@@ -1,1 +1,2 @@
 from .dsfp import *
+__VERSION__ = (0, 0, 1)
---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+import os
+import sys
+
+py_version = sys.version_info
+version = ".".join([str(i) for i in __import__('dsfp').__VERSION__])
+readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+
+CLASSIFIERS = [
+    'Development Status :: 1 - Alpha',
+    'Environment :: Darksouls Save File Parser',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+]
+
+install_requires = [
+    'simplejson',
+    'six'
+]
+
+
+if isinstance(py_version, tuple):
+    if py_version < (2, 7):
+        #if py_version== 2 and py_version.minor < 7:
+        install_requires.append('importlib')
+
+
+setup(
+    name='dsfp',
+    author='Nickolas Fox <tarvitz@blacklibrary.ru>',
+    version=version,
+    author_email='tarvitz@blacklibrary.ru',
+    #url='http://pypi.python.org/pypi/dsfp',
+    download_url='https://github.com/tarvitz/dsfp/archive/master.zip',
+    description='Darksouls Save File Parser for fetching data issues',
+    long_description=open(readme).read(),
+    license='MIT license',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=install_requires,
+    packages=find_packages(exclude=[]),
+    include_package_data=True,
+    zip_safe=False)
