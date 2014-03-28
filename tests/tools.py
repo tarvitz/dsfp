c4d05d6698b45d738f03e8fc75ca7db54f280662
Tarvitz <tarvitz@blacklibrary.ru>
2014-03-28 04:16:55 +0400

    update diff tests

--- tests/tools.py
+++ tests/tools.py
@@ -11,9 +11,15 @@ class TestBinDiff(TestCase):
     maxDiff = None
     diff_a = six.b('\x00\x01\x00\x04\x44\x53\x60\x34\x90\x88\x13\x08')
     diff_b = six.b('\x01\x00\x00\x04\x42\x53\x61\x34\x93\x88\x13\x08')
-    skip_table = {}
+    skip_table = [
+        {"comment": "unknown data", "offset": 0, "size": 2},
+    ]
     diff = [
         {'diff': [0, 1], 'offset': 0},
+        {'diff': [0, 2], 'offset': 4},
+        {'diff': [0], 'offset': 8}
+    ]
+    diff_patched = [
         {'diff': [0, 2], 'offset': 4},
         {'diff': [0], 'offset': 8}
     ]
@@ -25,3 +31,9 @@ class TestBinDiff(TestCase):
         diff_obj = BinDiff(self.diff_a, self.diff_b)
         diff = diff_obj.process_diff()
         self.assertEqual(diff, self.diff)
+
+    def test_bin_diff_patch_table(self):
+        diff_obj = BinDiff(self.diff_a, self.diff_b,
+                           skip_table=self.skip_table)
+        diff = diff_obj.process_diff()
+        self.assertEqual(diff, self.diff_patched)
